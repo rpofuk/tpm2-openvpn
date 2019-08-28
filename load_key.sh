98d74c84b213a5821b67348b158b6c7413abd172
Robert Pofuk <rpofuk@gmail.com>
2019-08-28 10:31:01 +0200

    Update load_key.sh

--- load_key.sh
+++ load_key.sh
@@ -2,11 +2,11 @@
 
 set -e 
 
-tpm2_createprimary -C o -c parent.ctx -G rsa2048:null:aes128cfb
+tpm2_clear
+tpm2_createprimary  -C o -c parent.ctx -G rsa2048:null:aes128cfb
 tpm2_evictcontrol -c parent.ctx
 
-tpm2_import -i private_key.key -r private_key.tss -u public_key.tss -Grsa -C parent.ctx
-tpm2_load -C parent.ctx -u public_key.tss -r private_key.tss -c wzhpor.ctx
+tpm2_import  -i $1 -r private_key.tss -u public_key.tss -Grsa -C parent.ctx
+tpm2_load  -C parent.ctx -u public_key.tss -r private_key.tss -c wzhpor.ctx
 tpm2_evictcontrol -c wzhpor.ctx
-
 
