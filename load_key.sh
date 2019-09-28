ca297d130905a368ce8e4bc429dff9bb1af82d9c
Robert Pofuk <rpofuk@gmail.com>
2019-09-28 08:46:02 +0200

    Update load_key.sh

--- load_key.sh
+++ load_key.sh
@@ -21,5 +21,16 @@ tpm2_import  -i $1 -r private_key.tss -u public_key.tss -Grsa -C parent.ctx
 tpm2_load  -C parent.ctx -u public_key.tss -r private_key.tss -c wzhpor.ctx
 tpm2_evictcontrol -c wzhpor.ctx
 
-npx @rpofuk/tpm2-asn-packer p ${HANDLE##*x} private_key.tss public_key.tss out.key
+npx @rpofuk/tpm2-asn-packer p ${HANDLE##*x} private_key.tss public_key.tss ewp401007.tss
+
+
+nmcli connection import type openvpn file ewp401007.ovpn
+nmcli connection modify ewp401007 ipv4.never-default yes
+nmcli connection modify ewp401007 ipv4.dns-search ewp-test.rbi.cloud
+nmcli connection modify ewp401007 +vpn.data key=$(realpath ewp401007.tss)
+nmcli connection modify ewp401007 +vpn.data cert=$(realpath ewp401007.crt)
+nmcli connection modify ewp401007 ipv6.method ignore
+
+
+
 
